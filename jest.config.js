208de5f31cba83ddb161c6fc426a43a88781de5c
Marcell Alves <marcell.alves@gmail.com>
2020-02-20 17:22:24 -0300

    chore: configure test debug

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
-};
+}
