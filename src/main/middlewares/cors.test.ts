148fc1504cd704f690b56f20e72e26bfab18da78
Marcell Alves <marcell.alves@gmail.com>
2020-02-21 11:50:00 -0300

    feat: ensure requests return json as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,11 +3,11 @@ import request from 'supertest'
 
 describe('CORS Middleware', () => {
   test('Should enable CORS', async () => {
-    app.post('/test_cors', (req, res) => {
-      res.send(req.body)
+    app.get('/test_cors', (req, res) => {
+      res.send()
     })
     await request(app)
-      .post('/test_cors')
+      .get('/test_cors')
       .expect('access-control-allow-origin', '*')
       .expect('access-control-allow-methods', '*')
       .expect('access-control-allow-headers', '*')
